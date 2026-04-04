5be9bd2e7997f4df3c8b288754ffeaa53e48177f
CeliaSerna <c123elia@gmail.com>
2026-04-04 23:40:34 +0200

    Cambio en index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const path = require('path');// le indicamos donde ir
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -9,9 +10,16 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', authRoutes);
 
+// Servir los archivos estáticos de la carpeta "dist" (donde Angular guarda la web compilada)
+app.use(express.static(path.join(__dirname, '../dist/liga-deportiva-calatrava/browser')));
+
+//Si se pide cualquier cosa que no sea /api, se manda al index.html de Angular
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/liga-deportiva-calatrava/browser/index.html'));
+});
+
 //CONEXIÓN A MONGODB 
-//ESTA RUTA NO USA SRV
-                                                                                                                                                                                        //FUNDAMENTAL PONER EL NOMBRE DE LA BASE DE DATOS ENTRE / Y ?
+//ESTA RUTA NO USA SRV                                                                                                                                                                                  //FUNDAMENTAL PONER EL NOMBRE DE LA BASE DE DATOS ENTRE / Y ?
 mongoose.connect('mongodb://c123elia:IESCalatrava@ac-2cswkyn-shard-00-00.opzwkjx.mongodb.net:27017,ac-2cswkyn-shard-00-01.opzwkjx.mongodb.net:27017,ac-2cswkyn-shard-00-02.opzwkjx.mongodb.net:27017/liga-deportiva-calatrava?ssl=true&replicaSet=atlas-q0iezm-shard-0&authSource=admin&appName=Cluster0')
 .then(() => console.log('MongoDB conectado'))
 .catch(err => console.log(err));
