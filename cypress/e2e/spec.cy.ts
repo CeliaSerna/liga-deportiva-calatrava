ee0103d65f373d8d7f934263eefcd53202d1cc48
CeliaSerna <c123elia@gmail.com>
2026-04-04 15:02:57 +0200

    Mocks en login en Cypress

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,35 +1,46 @@
-describe('Pruebas E2E', () => {
+describe('Pruebas E2E de Login (Simuladas para CI)', () => {
 
   beforeEach(() => {
     // 1. Entramos en la página de Login
-    cy.visit('http://localhost:4200/login'); 
+    cy.visit('/login'); 
   });
 
-  it('Caso 1: muestra error con credenciales incorrectas (Conexión real con Node.js)', () => {
-    // 2. Escribo datos que NO existen en MongoDB
+  it('Caso 1: muestra error con credenciales incorrectas', () => {
+   //1 Interceptamos la llamada al backend y devolvemos un error 401 (Unauthorized)
+    cy.intercept('POST', '**/api/login', {
+      statusCode: 401,
+      body: { message: 'Unauthorized' }
+    }).as('loginFail');
+    
     cy.get('input[name="usuario"]').type('usuario_falso');
     cy.get('input[name="password"]').type('password_incorrecta');
+    cy.get('button[type="submit"]').click(); //Hacemos clic en el botón de entrar
 
-    // 3. Hacemos clic en el botón de entrar
-    cy.get('button[type="submit"]').click();
-
-    // 4. Verificamos que el backend responde con un error 401 (Unauthorized)
-    // El mensaje 'Unauthorized' es el que suele devolver Node por defecto.
+    // Esperamos a que la petición ocurra
+    cy.wait('@loginFail');
+    // Verificamos el error 401 (Unauthorized)
     cy.contains('Unauthorized').should('be.visible');
   });
 
-  it('Caso 2: Debería entrar correctamente con un usuario real de MongoDB', () => {
-    // 1. Escribo los datos de UN USUARIO REAL DE MONGO
+  it('Caso 2: Debería entrar correctamente con un usuario simulado', () => {
+  // 2 Interceptamos la llamada y devolvemos un TOKEN falso exitoso
+    cy.intercept('POST', '**/api/login', {
+      statusCode: 200,
+      body: { 
+        token: 'token-falso-de-prueba',
+        user: { nombre: 'Admin', rol: 'admin' }
+      }
+    }).as('loginSuccess');    
+
     cy.get('input[name="usuario"]').type('Marta'); 
     cy.get('input[name="password"]').type('1234');
+    cy.get('button[type="submit"]').click(); //Pulsamos el botón
 
-    // 2. Pulsamos el botón
-    cy.get('button[type="submit"]').click();
+    // Esperamos a la respuesta exitosa
+    cy.wait('@loginSuccess');
 
-    // 3. VERIFICACIÓN DE ÉXITO: 
     // Si al loguearte te redirige a otra página, comprobamos la URL:
-    //uso 'include' y no 'eq' poruqe con el metodo get la url añadira los datos del usuario 
-    cy.url().should('include', 'http://localhost:4200/rol'); 
+    cy.url().should('include', '/rol'); 
     
     // O si sale un mensaje de bienvenida:
     // cy.contains('Bienvenid@').should('be.visible');
