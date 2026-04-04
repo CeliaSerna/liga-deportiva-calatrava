e3c170802cadb295907d2e0037901f88554da245
CeliaSerna <c123elia@gmail.com>
2026-04-05 00:57:02 +0200

    3º Editar * index.js

--- backend/index.js
+++ backend/index.js
@@ -13,11 +13,6 @@ app.use('/api', authRoutes);
 // Servir los archivos estáticos de la carpeta "dist" (donde Angular guarda la web compilada)
 app.use(express.static(path.join(__dirname, '../dist/liga-deportiva-calatrava/browser')));
 
-//Si se pide cualquier cosa que no sea /api, se manda al index.html de Angular
-app.get('/*', (req, res) => {// hay que poner el * en comillas porque da error en express
-  res.sendFile(path.join(__dirname, '../dist/liga-deportiva-calatrava/browser/index.html'));
-});
-
 //CONEXIÓN A MONGODB 
 //ESTA RUTA NO USA SRV                                                                                                                                                                                  //FUNDAMENTAL PONER EL NOMBRE DE LA BASE DE DATOS ENTRE / Y ?
 mongoose.connect('mongodb://c123elia:IESCalatrava@ac-2cswkyn-shard-00-00.opzwkjx.mongodb.net:27017,ac-2cswkyn-shard-00-01.opzwkjx.mongodb.net:27017,ac-2cswkyn-shard-00-02.opzwkjx.mongodb.net:27017/liga-deportiva-calatrava?ssl=true&replicaSet=atlas-q0iezm-shard-0&authSource=admin&appName=Cluster0')
@@ -36,4 +31,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor backend funcionando en el puerto ${PORT}`);
 });
- 
+
+app.use((req, res) => { //lo mando a esta url como ultima solución
+  res.sendFile(path.join(__dirname, '../dist/liga-deportiva-calatrava/browser/index.html'));
+});
