82f22f20f59b2498f304d75cacfd600c98d6877f
CeliaSerna <c123elia@gmail.com>
2026-04-05 17:12:48 +0200

    Pongo una lista fija de jugadores

--- src/app/components/jugadores/jugadores.ts
+++ src/app/components/jugadores/jugadores.ts
@@ -8,16 +8,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './jugadores.html',
   styleUrl: './jugadores.css',
 })
-export class JugadoresComponent implements OnInit {
-  jugadores: any[] = [];
+export class JugadoresComponent implements OnInit { //lista fija de jugadores para no tener que esperar al servidor
+  jugadores: any[] = [
+    { id: '1', nombre: 'Luis Gómez', equipo: 'Tigres FC' },
+    { id: '2', nombre: 'Carlos Soto', equipo: 'Leones Club' },
+    { id: '3', nombre: 'Mario Ruiz', equipo: 'Águilas Juveniles' },
+    { id: '4', nombre: 'Iván Torres', equipo: 'Halcones Club' },
+    { id: '5', nombre: 'Nerea Camacho', equipo: 'Coyotes Junior' },
+  ];
 
   // INYECTAMOS EL SERVICIO (Comunicación Componente-Servicio)
   constructor(private jugadoresService: JugadoresService) {}
 
   ngOnInit(): void {
-    // El componente le pide los datos al servicio
-    this.jugadoresService.getJugadores().subscribe(data => {
-      this.jugadores = data;
+    //Mantenemos la llamada al servicio pero NO sobrescribimos la lista 
+    this.jugadoresService.getJugadores().subscribe({
+      next: (data) => {
+        if (data && data.length > 0) {
+          this.jugadores = data; // Si el servidor responde, usa los jugadores del servidor
+        }
+      },
+      error: (err) => {
+        console.log('Usando lista fija de reserva'); // Si el servidor falla, la lista de arriba se queda puesta
+      }
     });
   }
 }
