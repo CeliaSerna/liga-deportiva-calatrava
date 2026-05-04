2bf020303078902272784ea7e1bb51e730dab612
c123e <c123elia@gmail.com>
2026-05-04 21:18:54 +0200

    routes/web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,11 @@
-<?php
+use Illuminate\Support\Facades\Artisan;
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/instalar-todo', function () {
+    try {
+        // Esto crea las tablas y mete los usuarios del Seeder
+        Artisan::call('migrate:fresh --seed --force');
+        return "Éxito: Tablas creadas y usuarios admin y lector listos.";
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
 });
