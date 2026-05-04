ff0d11d05154f1c79393fed3468d9ac97ad4b455
c123e <c123elia@gmail.com>
2026-05-04 18:55:22 +0200

    tipos de usuarios

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // 1. USUARIO ADMINISTRADOR (Con permisos para escribir)
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@liga-calatrava.com',
+            'password' => Hash::make('admin1234'),
+            'role' => 1, 
+        ]);
+
+        // 2. USUARIO SIN PERMISOS (Solo lectura, no puede escribir)
+        User::create([
+            'name' => 'Usuario',
+            'email' => 'user@liga-calatrava.com',
+            'password' => Hash::make('user1234'),
+            'role' => 0, 
+        ]);
+    }
+}
